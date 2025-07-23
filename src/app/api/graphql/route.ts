068afd69121df58a0d61fa6f1ca2c0186b24e89f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-07-24 00:06:18 +0100

    feat: enable gql playground
    feat: use zero installs
    
    feat: compress dependencies

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -10,12 +10,24 @@ import { CollectionLogAPI } from './datasources/collection-log.datasource';
 import type { Context } from './context';
 import { PlayerCollectionLogResolver } from './collection-log/player-collection-log.resolver';
 import { NextRequest } from 'next/server';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
 
 const schema = await buildSchema({
   resolvers: [PlayerResolver, PlayerCollectionLogResolver],
 });
 
-const server = new ApolloServer<Context>({ schema });
+const server = new ApolloServer<Context>({
+  schema,
+  plugins: [
+    process.env.SHOW_GRAPHQL_PLAYGROUND === 'true' ||
+    process.env.NODE_ENV !== 'production'
+      ? ApolloServerPluginLandingPageLocalDefault()
+      : ApolloServerPluginLandingPageProductionDefault(),
+  ],
+});
 
 const handler = startServerAndCreateNextHandler<NextRequest, Context>(server, {
   context: async (req) => {
